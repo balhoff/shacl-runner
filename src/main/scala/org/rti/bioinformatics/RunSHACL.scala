a4f14069319f0577cc30f0c5a9282fbd04b4d0ee
Jim Balhoff <jim@balhoff.org>
2016-06-23 12:28:44 -0400

    Updated validation runner. Added check altID points to a non-obsolete term.

--- src/main/scala/org/rti/bioinformatics/RunSHACL.scala
+++ src/main/scala/org/rti/bioinformatics/RunSHACL.scala
@@ -2,20 +2,19 @@ package org.rti.bioinformatics
 
 import java.net.URI
 import java.util.UUID
+
+import scala.collection.JavaConverters._
+
+import org.apache.jena.graph.compose.MultiUnion
 import org.apache.jena.rdf.model.ModelFactory
+import org.apache.jena.util.FileUtils
+import org.topbraid.shacl.arq.SHACLFunctions
 import org.topbraid.shacl.constraints.ModelConstraintValidator
 import org.topbraid.shacl.util.ModelPrinter
-import org.topbraid.spin.arq.ARQFactory
-import scala.collection.JavaConverters._
-import org.topbraid.shacl.arq.SHACLFunctions
-import org.topbraid.spin.util.JenaUtil
-import org.apache.jena.graph.compose.MultiUnion
-import java.io.InputStream
-import org.apache.jena.util.FileUtils
 import org.topbraid.shacl.vocabulary.SH
-import org.apache.jena.graph.Graph
+import org.topbraid.spin.arq.ARQFactory
+import org.topbraid.spin.util.JenaUtil
 import org.topbraid.spin.util.SystemTriples
-import org.topbraid.shacl.util.SHACLUtil
 
 object RunSHACL extends App {
 
@@ -41,17 +40,19 @@ object RunSHACL extends App {
     shaclModel.getGraph,
     shapesModel.getGraph))
   val unionShapesModel = ModelFactory.createModelForGraph(unionGraph)
-  val unionShapesModelUpdated = SHACLUtil.withDefaultValueTypeInferences(unionShapesModel)
 
   // Register core SHACL functions
-  SHACLFunctions.registerFunctions(unionShapesModelUpdated)
+  SHACLFunctions.registerFunctions(unionShapesModel)
 
   val shapesGraphURI = URI.create("urn:x-shacl-shapes-graph:" + UUID.randomUUID.toString)
-  dataset.addNamedModel(shapesGraphURI.toString, unionShapesModelUpdated)
+  dataset.addNamedModel(shapesGraphURI.toString, unionShapesModel)
 
   // Validate dataset against constraints in shapes graph
   val results = ModelConstraintValidator.get.validateModel(dataset, shapesGraphURI, null, false, null)
   // Need a much better result printer
-  println(ModelPrinter.get.print(results))
+  if (!results.isEmpty()) {
+    println(ModelPrinter.get.print(results))
+    System.exit(1)
+  }
 
 }
